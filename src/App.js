a70ab7ebd70f3dacc25ac9eae63c51c87d2fac89
veekram <veekramsuwal@gmail.com>
2019-11-15 14:19:56 +0545

    Dual Axis Chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FusionCharts from "fusioncharts";
+import TimeSeries from "fusioncharts/fusioncharts.timeseries";
+import ReactFC from "react-fusioncharts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ReactFC.fcRoot(FusionCharts, TimeSeries);
+
+const jsonify = res => res.json();
+const dataFetch = fetch(
+  "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/data/dual-y-axis-data.json"
+).then(jsonify);
+const schemaFetch = fetch(
+  "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/schema/dual-y-axis-schema.json"
+).then(jsonify);
+
+const dataSource = {
+  chart: {
+    multicanvas: false
+  },
+  caption: {
+    text: "Energy & Temperature Measurements"
+  },
+  yaxis: [
+    {
+      plot: [
+        {
+          value: "Energy",
+          connectnulldata: true,
+          type: "line"
+        }
+      ],
+      format: {
+        suffix: " kWh"
+      }
+    },
+    {
+      plot: [
+        {
+          value: "Temperature",
+          connectnulldata: true,
+          type: "line"
+        }
+      ],
+      format: {
+        suffix: " °C"
+      }
+    }
+  ]
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onFetchData = this.onFetchData.bind(this);
+    this.state = {
+      timeseriesDs: {
+        type: "timeseries",
+        renderAt: "container",
+        width: "600",
+        height: "400",
+        dataSource
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.onFetchData();
+  }
+
+  onFetchData() {
+    Promise.all([dataFetch, schemaFetch]).then(res => {
+      const data = res[0];
+      const schema = res[1];
+      const fusionTable = new FusionCharts.DataStore().createDataTable(
+        data,
+        schema
+      );
+      const timeseriesDs = Object.assign({}, this.state.timeseriesDs);
+      timeseriesDs.dataSource.data = fusionTable;
+      this.setState({
+        timeseriesDs
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.timeseriesDs.dataSource.data ? (
+           <ReactFC {...this.state.timeseriesDs} />
+        ) : (
+           "loading"
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
